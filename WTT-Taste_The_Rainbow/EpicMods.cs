e88eb770b8f8a1cc1165103a54402b8da0cedb22
EpicRangeTime <48403096+EpicRangeTime@users.noreply.github.com>
2026-03-29 14:41:44 -0600

    Initial commit

---
+++ WTT-Taste_The_Rainbow/EpicMods.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using Range = SemanticVersioning.Range;
+
+namespace WTT_Rainbow;
+
+public record ModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "com.epicrangetime.rainbow";
+    public override string Name { get; init; } = "WTT-Taste The Rainbow";
+    public override string Author { get; init; } = "EpicRangeTime";
+    public override List<string>? Contributors { get; init; } = null;
+    public override SemanticVersioning.Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version?.ToString(3));
+    public override Range SptVersion { get; init; } = new("~4.0.13");
+    public override List<string>? Incompatibilities { get; init; }
+    public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
+    {
+        { "com.wtt.commonlib", new Range("~2.0.15") }
+    };
+    public override string? Url { get; init; }
+    public override bool? IsBundleMod { get; init; } = true;
+    public override string License { get; init; } = "CC-BY-NC-ND 4.0";
+}
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 2)]
+public class WTT_RainbowMags(
+    WTTServerCommonLib.WTTServerCommonLib wttCommon) : IOnLoad
+{
+    public async Task OnLoad()
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
+    }
+}
